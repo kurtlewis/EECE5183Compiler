f54e284b8ee2a98557f268fce4fedf9fe16554f5
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-01-28 17:42:00 -0500

    Add TOKEN enum

--- include/kjlc/scanner.h
+++ include/kjlc/scanner.h
@@ -9,6 +9,55 @@
 
 namespace kjlc {
 
+enum TOKEN {
+  // reserved words
+  BEGIN, // "begin"
+  ELSE, // "else"
+  END, // "end"
+  FALSE, // "false"
+  FOR, // "for"
+  GLOBAL, // "global"
+  IF, // "if"
+  IS, // "is"
+  NOT, // "not"
+  PROCEDURE, // "procedure"
+  PROGRAM, // "program"
+  RETURN, // "return"
+  THEN, // "then"
+  TRUE, // "true"
+  TYPE, // "type"
+  VARIABLE, // "variable"
+  // character tokens
+  BRACK_LEFT, // "["
+  BRACK_RIGHT, // "]"
+  COMMA, // ","
+  CURLY_LEFT, // "{"
+  CURLY_RIGHT, // "}"
+  PAREN_LEFT, // "("
+  PAREN_RIGHT, // ")"
+  PERIOD, // "."
+  SEMI_COLON, // ";"
+  // operators
+  AND, // "&"
+  DIV, // "/"
+  EQ, // "=="
+  GT, // ">"
+  GT_EQ, // ">="
+  LT, // "<"
+  LT_EQ, // "<="
+  MINUS, // "-"
+  MULT, // "*"
+  NEQ, // "!="
+  PLUS, // "+"
+  // types
+  BOOL, // "bool"
+  ENUM, // "enum"
+  FLOAT, // "float"
+  INTEGER, // "integer"
+  STRING, // "string"
+};
+
+
 class Scanner {
   public:
     Scanner();
