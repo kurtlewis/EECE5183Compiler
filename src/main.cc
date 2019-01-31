e6ab1610038079f9f6effe155ba229e03e700337
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-01-31 14:50:35 -0500

    Add printing out of lexemes to main

--- src/main.cc
+++ src/main.cc
@@ -17,6 +17,21 @@ int main(int argc, char* argv[]) {
   }
   std::cout << "Entered filename: " << argv[1] << std::endl;
   kjlc::Scanner scan(argv[1]);
+  bool quit = false;
+  while (!quit) {
+    kjlc::Lexeme lexeme = scan.ScanNextLexeme();
+    if (lexeme.token == kjlc::T_ID || lexeme.token == kjlc::T_QUOTE) {
+      std::cout << lexeme.str_value;
+    } else  if (lexeme.token ==  kjlc::T_INT_LITERAL) {
+      std::cout << lexeme.int_value;
+    } else if (lexeme.token == kjlc::T_FLOAT_LITERAL) {
+      std::cout << lexeme.float_value;
+    } else {
+      std::cout << "tok: " << lexeme.token;
+    }
+    if (lexeme.token == kjlc::T_PERIOD) quit = true;
+    std::cout << std::endl;
+  }
   while (scan.ScanNextLexeme().token != kjlc::T_PERIOD);
   return 0;
 }
