2244d93a9be9904a86e5fa0f73237a9fa45c6fda
Kurt Lewis <kurt.jlewis15@gmail.com>
2019-03-06 14:08:30 -0500

    Begin adding API for SymbolTable

--- include/kjlc/symbol_table.h
+++ include/kjlc/symbol_table.h
@@ -7,9 +7,43 @@
 #ifndef EECE5138COMPILER_KJLC_SYMBOL_TABLE_H
 #define EECE5138COMPILER_KJLC_SYMBOL_TABLE_H
 
+#include <map>
+#include <string>
+#include <vector>
+
 namespace kjlc {
 
+enum Type {
+  TYPE_BOOL,
+  TYPE_ENUM,
+  TYPE_FLOAT,
+  TYPE_INT,
+  TYPE_STRING
+};
+
+struct Symbol {
+  std::string id;
+  Type type;
+};
+
 class SymbolTable {
+  public:
+    // Constructor initializes symbol table
+    SymbolTable();
+
+    ~SymbolTable();
+
+    void IncreaseScope();
+
+    void DecreaseScope();
+
+    Symbol FindSymbolByIdentifier(std::string id);
+
+  private:
+    // a stack of scope for local scope
+    std::vector<std::map<std::string, Symbol> > local_scope_map_;
+    // a single map of global scope identifiers
+    std::map<std::string, Symbol> global_scope_map_;
 
 };
 
--- src/symbol_table.cc
+++ src/symbol_table.cc
@@ -6,8 +6,24 @@
  **/
 #include "kjlc/symbol_table.h"
 
-
 namespace kjlc {
 
+SymbolTable::SymbolTable() {
+}
+
+SymbolTable::~SymbolTable() {
+}
+
+void SymbolTable::IncreaseScope() {
+  // todo
+}
+
+void SymbolTable::DecreaseScope() {
+  // todo
+}
+
+Symbol SymbolTable::FindSymbolByIdentifier(std::string id) {
+  // todo
+}
 
 } // namespace kjlc
